82647dab08408f629cebb8ce53c91b66a128c9be
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-07-20 23:04:20 +0300

    Initial commit

---
+++ src/main/java/codenamed/flavored/FlavoredClient.java
@@ -0,0 +1,10 @@
+package codenamed.flavored;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlavoredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/codenamed/flavored/FlavoredDataGenerator.java
@@ -0,0 +1,11 @@
+package codenamed.flavored;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlavoredDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
