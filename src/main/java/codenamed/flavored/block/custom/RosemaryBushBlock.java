70ee10b1507a22b7191a44331456ac704e56f142
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-07-26 12:44:09 +0300

    Rosemary fix

--- src/main/java/codenamed/flavored/block/custom/RosemaryBushBlock.java
+++ src/main/java/codenamed/flavored/block/custom/RosemaryBushBlock.java
@@ -8,9 +8,12 @@ import net.minecraft.block.SweetBerryBushBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -24,11 +27,6 @@ public class RosemaryBushBlock extends SweetBerryBushBlock implements Fertilizab
         super(settings);
     }
 
-    @Override
-    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {BlockPos down = pos.down();BlockState blockState = world.getBlockState(down);
-        return blockState.isOf(Blocks.FARMLAND);
-    }
-
 
     @Override
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
@@ -37,7 +35,7 @@ public class RosemaryBushBlock extends SweetBerryBushBlock implements Fertilizab
 
 
     public ItemStack getPickStack(WorldView world, BlockPos pos, BlockState state) {
-        return new ItemStack(FlavoredItems.PEPPER_SEEDS);
+        return new ItemStack(FlavoredItems.ROSEMARY);
     }
 
 
@@ -57,4 +55,21 @@ public class RosemaryBushBlock extends SweetBerryBushBlock implements Fertilizab
         }
     }
 
+    @Override
+    protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        int i = (Integer)state.get(AGE);
+        boolean bl = i == 3;
+        if (i > 1 && itemStack.isOf(Items.SHEARS)) {
+            int j = 1 + world.random.nextInt(2);
+            dropStack(world, pos, new ItemStack(FlavoredItems.ROSEMARY, j + (bl ? 1 : 0)));
+            world.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
+            BlockState blockState = (BlockState)state.with(AGE, 1);
+            world.setBlockState(pos, blockState, 2);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
+            return ItemActionResult.SUCCESS;
+        }
+        return ItemActionResult.FAIL;
+
+    }
 }
